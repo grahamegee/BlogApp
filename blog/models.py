8acb4f66ca21433865b850ef36c77487841039ab
Grahame Gardiner <grahamegee@gmail.com>
2014-10-16 21:40:10 +0100

    remove currently useless entry attribute.

--- blog/models.py
+++ blog/models.py
@@ -8,11 +8,6 @@ class Entry(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now())
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __unicode__(self):
         return self.title

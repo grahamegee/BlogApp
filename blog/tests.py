4ce2dbbf6bc9082359c2de20e6e739a5bde216c4
Grahame Gardiner <grahamegee@gmail.com>
2014-10-21 00:54:58 +0100

    made a start on unti testing

--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from .models import Entry
 
+class Test(TestCase):
+    def test_check(self):
+        self.assertEqual(1+1, 2)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class EntryModelTests(TestCase):
+    
+    def test_created_time_updated_on_add(self):
         """
-        Tests that 1 + 1 always equals 2.
+        The creation time should be set to timezone.now() when a blog entry is
+        created.
         """
-        self.assertEqual(1 + 1, 2)
+    # testing this slighty indirectly by checking that if two entries are created
+    # in sequence, the second one has a later creation_time. Hard to test that
+    # creation time == timezone.now() because of time delay between creating and
+    # testing.
+        first_entry = Entry.objects.create(title="title 1", text='text 1')
+        second_entry = Entry.objects.create(title="title 2", text='text 2')
+        print first_entry.created_date
+        print second_entry.created_date
+
+        self.assertEqual(first_entry.created_date < second_entry.created_date,
+                         True)

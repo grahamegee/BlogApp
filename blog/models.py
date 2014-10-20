8dabe6e428fa896712213011ec732946d8c8f467
Grahame Gardiner <grahamegee@gmail.com>
2014-10-20 16:06:42 +0100

    sorted nav bar, add new button, and fixed a slight issue with displaying posts in the wrong order.

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,12 @@ from django.contrib.auth.models import User
 class Entry(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
-
+    # only updates created date on add (not edit)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created_date']
+    
     def __unicode__(self):
         return self.title
         
